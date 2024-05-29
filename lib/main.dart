3f6bca0f6087e1886b53213520c0f276ba717dd8
Ivan <apedoivan@gmail.com>
2024-05-29 09:58:07 +0200

    little change

--- lib/main.dart
+++ lib/main.dart
@@ -40,15 +40,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: VirtualAquarium(),
-    );
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //     useMaterial3: true,
+    //   ),
+    //   debugShowCheckedModeBanner: false,
+    //   home: VirtualAquarium(),
+    // );
 
     // return ChangeNotifierProvider(
     //   create: (context) => PuzzleProvider(),
@@ -74,16 +74,16 @@ class MyApp extends StatelessWidget {
     //   ),
     // );
 
-    // return ChangeNotifierProvider(
-    //   create: (context) => CartModel(),
-    //   child: MaterialApp(
-    //     title: 'Shopping Cart',
-    //     theme: ThemeData(
-    //       primarySwatch: Colors.blue,
-    //     ),
-    //     debugShowCheckedModeBanner: false,
-    //     home: ShoppingPage(),
-    //   ),
-    // );
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: MaterialApp(
+        title: 'Shopping Cart',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: ShoppingPage(),
+      ),
+    );
   }
 }
