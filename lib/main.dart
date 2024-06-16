8c0496f1c216e8c0eccbd0bc7cffbff2986c03c1
Ivan <apedoivan@gmail.com>
2024-06-16 23:41:00 +0200

    little change

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class MyApp extends StatelessWidget {
     //     useMaterial3: true,
     //   ),
     //   debugShowCheckedModeBanner: false,
-    //   home: DisintegrateImageAnimation(),
+    //   home: GradientBackgroundWithAnimation(),
     // );
 
     // return ChangeNotifierProvider(
